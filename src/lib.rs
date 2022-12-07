e9afc497c60f4d4f4ec519e4c0cc4fba3eb030be
msc22h12 Noah Zarro (zarron) <msc22h12@badile18.ee.ethz.ch>
2022-12-07 19:14:33 +0100

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+#![no_std]
+
+use core::arch::asm;
+
+fn print(text: &str) {
+    let print_addr = 0x1A10FF80;
+
+    for byte in text.as_bytes() {
+        unsafe {
+            asm!(
+                "sw	{1},0({0})",
+                in(reg) print_addr,
+                in(reg) *byte as u32,
+            );
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! println {
+    ($($arg:tt),*) => {{
+        $(
+            print($arg);
+            print(" ");
+        )*
+        print("\n");
+
+    }};
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt),*) => {{
+        $(
+            print($arg);
+            print(" ");
+        )*
+
+    }};
+}
